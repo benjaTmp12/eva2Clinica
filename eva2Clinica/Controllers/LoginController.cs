046a18896051514e19000facfa1c7e46909932fc
javifabi <j.fabia@alumnos.santotomas.cl>
2026-05-26 19:41:29 -0400

    Create LoginController.cs

---
+++ eva2Clinica/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eva2Clinica.Models;
+
+namespace eva2Clinica.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel login)
+        {
+            if (login.Usuario == "admin" && login.Clave == "1234")
+            {
+                HttpContext.Session.SetString("Usuario", login.Usuario);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+            return View(login);
+        }
+    }
+}
