df24c83edbb6897e6dda804af4e466f1494ba44c
javifabi <j.fabia@alumnos.santotomas.cl>
2026-05-26 19:41:32 -0400

    Create LogininViewModel.cs

---
+++ eva2Clinica/Models/LogininViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eva2Clinica.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Ingrese el usuario")]
+        public string Usuario { get; set; } = "";
+
+        [Required(ErrorMessage = "Ingrese la contraseña")]
+        public string Clave { get; set; } = "";
+    }
+}
